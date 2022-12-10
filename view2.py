3353c5392e16ba76d3dc2838455ee4f4f2684e56
LukeDFlynn <118565875+LukeDFlynn@users.noreply.github.com>
2022-12-09 21:21:34 -0600

    Project 2 Files

--- view2.py
+++ view2.py
@@ -14,7 +14,6 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(373, 191)
         MainWindow.setFixedSize(373, 191)
         MainWindow.setStyleSheet("background: url(:/Downloads/TwiceNow.png)")
         self.centralwidget = QtWidgets.QWidget(MainWindow)
